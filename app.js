2ce7ab4eea7f967679d5fd227f55dbdb66b18f9d
GonzaloCedillo <goncedillo@gmail.com>
2018-06-02 14:58:09 +0200

    Comprobando si algo es porno, pero no es fiable :(

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("Hola Clase")
+const isPorn = require("is-porn");
+const url = "http://pornhub.com";
+
+isPorn(url, (err, status) => console.log(`¿El site ${url} es porno?: ${status ? 'Sí' : 'No'}`))
